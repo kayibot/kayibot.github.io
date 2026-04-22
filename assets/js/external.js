05384c09cc77480a928781797390a06f160631a8
kayi <kayi>
2026-04-22 12:57:07 +0300

    feat: delay bot manager UI initialization with a 1.7s timeout

--- assets/js/external.js
+++ assets/js/external.js
@@ -32,6 +32,39 @@ function loadJS(FILE_URL) {
 
 (function (wHandle, wjQuery) {
 
+    setTimeout(() => {
+        return createLoginUI();
+    }, 1700);
+    function createLoginUI() {
+
+
+        // window kirliliği yapmadan, lokal (closure) içinde manager objemizi üretiyoruz
+        const botManager = new BotManager();
+
+        const container = document.createElement('div');
+        container.style.position = 'absolute';
+        container.style.top = '10px';
+        container.style.left = '10px';
+        container.style.zIndex = '999999';
+
+        const btn = document.createElement('button');
+        btn.innerText = '3 Adet İzleyici (Bot) Yolla';
+        btn.style.padding = '10px 20px';
+        btn.style.fontSize = '14px';
+        btn.style.cursor = 'pointer';
+        btn.style.backgroundColor = '#2c3e50';
+        btn.style.color = '#fff';
+        btn.style.border = 'none';
+        btn.style.borderRadius = '5px';
+
+        container.appendChild(btn);
+        document.body.appendChild(container);
+
+        btn.addEventListener('click', () => {
+            botManager.spawnBots(3); // Butona basıldığında 3 WebSocket bağlantısı açılır
+        });
+
+    }
 
     // =======================================================
     // 0. WEBSOCKET HOOK (Araya Girme)
@@ -2907,31 +2940,6 @@ $(document).ready(function () {
     }
 
 
-    // window kirliliği yapmadan, lokal (closure) içinde manager objemizi üretiyoruz
-    const botManager = new BotManager();
-
-    const container = document.createElement('div');
-    container.style.position = 'absolute';
-    container.style.top = '10px';
-    container.style.left = '10px';
-    container.style.zIndex = '999999';
-
-    const btn = document.createElement('button');
-    btn.innerText = '3 Adet İzleyici (Bot) Yolla';
-    btn.style.padding = '10px 20px';
-    btn.style.fontSize = '14px';
-    btn.style.cursor = 'pointer';
-    btn.style.backgroundColor = '#2c3e50';
-    btn.style.color = '#fff';
-    btn.style.border = 'none';
-    btn.style.borderRadius = '5px';
-
-    container.appendChild(btn);
-    document.body.appendChild(container);
-
-    btn.addEventListener('click', () => {
-        botManager.spawnBots(3); // Butona basıldığında 3 WebSocket bağlantısı açılır
-    });
 
 
 });
